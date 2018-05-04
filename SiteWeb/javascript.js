882a20895f8d02b779ab218166ed81d129b9e71f
Tomy Roy <tomyroy19@live.ca>
2018-05-04 01:36:17 -0400

    Changer l'animation

--- SiteWeb/javascript.js
+++ SiteWeb/javascript.js
@@ -1,19 +1,19 @@
 window.onload = function () {
 
   document.getElementById("wrapper1").addEventListener("click", toggle);
-  
+
   document.getElementById("wrapper2").addEventListener("click", toggle2);
 
 }
 
 function toggle() {
 
-  document.getElementById("paragraphe1id").classList.toggle("hidden");
+  document.getElementById("paragraphe1id").classList.toggle("nothidden");
 
 }
 
 function toggle2() {
 
-  document.getElementById("paragraphe2id").classList.toggle("hidden");
+  document.getElementById("paragraphe2id").classList.toggle("nothidden");
 
 }
